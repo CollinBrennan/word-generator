ab438b5d1dbda5394df7c334e4d21be532f4408d
Collin Brennan <collinbrennan8@gmail.com>
2023-11-26 09:59:42 -0500

    Simplified padding

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,18 +6,18 @@ function Home() {
   const [output, setOutput] = useState<Inputs>()
   return (
     <div className="flex justify-center">
-      <div className="w-full max-w-screen-xl justify-center items-center flex px-2">
-        <div className="flex flex-col md:flex-row w-full gap-2 pt-2">
-          <div className="rounded-xl p-2 md:w-2/3">
+      <div className="w-full max-w-screen-xl justify-center flex pt-8">
+        <div className="flex flex-col md:flex-row w-full p-2 gap-4">
+          <div className="rounded-xl md:w-2/3">
             <Form setOutput={setOutput} />
           </div>
 
-          <div className="p-2 md:w-1/3 flex flex-col gap-4">
-            <div className="bg-secondary/50 rounded-xl shadow p-4 flex justify-between">
+          <div className="md:w-1/3 flex flex-col gap-4">
+            <div className="bg-secondary/50 rounded-xl shadow flex justify-between p-4">
               IPA Chart <ChevronDownIcon className="h-6" />
             </div>
             <div className="bg-primary rounded-xl shadow p-4">
-              <p className="pb-4 font-bold">Words: </p>
+              <p className="font-bold">Words: </p>
               <p className="overflow-hidden">
                 {output?.pattern ?? (
                   <span className="italic">
