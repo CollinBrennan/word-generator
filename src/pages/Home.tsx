d78971f60ca7f3f5d963966a3be047181b31cac1
Collin Brennan <collinbrennan8@gmail.com>
2023-11-27 13:47:08 -0500

    Added basic mobile layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,18 +10,17 @@ function Home() {
   const wordList = formData ? generateWordList(formData) : null
 
   return (
-    <div className="flex">
-      <div className="flex flex-col bg-secondary/50">
+    <div className="flex flex-col md:flex-row">
+      <div className="flex-col shrink-0 bg-secondary/25 hidden md:flex">
         {isChartShowing ? (
           <>
             <div className="bg-secondary p-4 flex justify-between">
               <h1>IPA Chart</h1>
               <button onClick={() => setIsChartShowing(false)}>
-                <XMarkIcon className="h-6" />
+                <XMarkIcon className="w-6" />
               </button>
             </div>
-
-            <div className="h-[calc(100vh-7rem)] overflow-y-scroll p-4">
+            <div className="h-[calc(100vh-7rem)] overflow-y-auto p-4">
               <IPAChart />
             </div>
           </>
@@ -38,15 +37,15 @@ function Home() {
       </div>
 
       <div className="flex flex-col bg-background flex-grow">
-        <h1 className="bg-neutral-100 p-4">Configuration</h1>
-        <div className="h-[calc(100vh-7rem)] overflow-y-scroll p-4">
+        <div className="bg-neutral-100 p-4">Configuration</div>
+        <div className="md:h-[calc(100vh-7rem)] overflow-y-auto p-4">
           <Form setFormData={setFormData} />
         </div>
       </div>
 
-      <div className="flex flex-col bg-primary/50 w-1/4">
+      <div className="flex flex-col bg-primary/25 md:w-1/4">
         <h1 className="bg-primary p-4">Words</h1>
-        <div className="h-[calc(100vh-7rem)] overflow-y-scroll p-4">
+        <div className="md:h-[calc(100vh-7rem)] overflow-y-auto p-4">
           <p className="whitespace-pre-line">
             {wordList ? (
               wordList.join('\n')
