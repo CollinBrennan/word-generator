5eee45569ec1ca4ee2a4e28c3b4c59c6ea69218b
Collin Brennan <collinbrennan8@gmail.com>
2023-12-03 13:05:42 -0500

    Updated logo and icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,16 @@
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import logo from '../assets/logo.svg'
 
 const pages = [
   { name: 'Generator', path: '/' },
   { name: 'Docs', path: '/docs' },
 ]
 
-const links = [{ name: 'Github', url: 'https://github.com/CollinBrennan' }]
+const links = [
+  { name: 'Github', url: 'https://github.com/CollinBrennan/word-generator' },
+]
 
 function Navbar() {
   const [currentPath, setCurrentPath] = useState('/')
@@ -26,8 +29,8 @@ function Navbar() {
   return (
     <div className="bg-neutral-700 text-background">
       <nav className="relative flex justify-between items-center px-4 py-2">
-        <h1 className="flex items-center text-neutral-300">
-          <span className="text-2xl font-bold text-background pr-4">wrdz</span>{' '}
+        <h1 className="flex items-center text-neutral-300 gap-4">
+          <img src={logo} className="h-6" />
           by Collin Brennan
         </h1>
 
