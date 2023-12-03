3b699d2674627af89c2c2cc6e2bfce7481365380
Collin Brennan <collinbrennan8@gmail.com>
2023-12-03 15:20:52 -0500

    Added output options to address #2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
 import Form from '../components/Form'
 import { generateWordList } from '../generate'
+import { ClipboardDocumentListIcon } from '@heroicons/react/24/outline'
 
 function Home() {
   const [wordList, setWordList] = useState<string[]>([])
+  const [showingDuplicates, setShowingDuplicates] = useState(true)
+  const [showingAsList, setShowingAsList] = useState(false)
+
+  const wordListNoDuplicates = [...new Set(wordList)]
+  const outputText = (showingDuplicates ? wordList : wordListNoDuplicates).join(
+    showingAsList ? '\n' : ' '
+  )
+
+  function handleWriteToClipboard() {
+    navigator.clipboard.writeText(outputText)
+  }
 
   return (
     <div className="flex flex-col md:flex-row">
@@ -11,14 +23,60 @@ function Home() {
         <Form onSubmit={(data) => setWordList(generateWordList(data))} />
       </div>
 
-      <div className="flex flex-col bg-primary/25 md:w-1/4">
+      <div className="flex flex-col bg-primary/20 md:w-1/4">
         <h1 className="bg-primary p-4">Words</h1>
         <div className="md:h-[calc(100vh-7rem)] overflow-y-auto p-4">
+          <div className="flex items-center justify-between">
+            <div className="flex gap-4">
+              <label className="flex items-center gap-1">
+                <input
+                  type="checkbox"
+                  onChange={() => setShowingAsList((prev) => !prev)}
+                  checked={showingAsList}
+                />
+                List
+              </label>
+
+              <label className="flex items-center gap-1">
+                <input
+                  type="checkbox"
+                  onChange={() => setShowingDuplicates((prev) => !prev)}
+                  checked={showingDuplicates}
+                />
+                Show duplicates
+              </label>
+            </div>
+
+            <div>
+              <button onClick={handleWriteToClipboard} className="p-2 group">
+                <ClipboardDocumentListIcon className="h-6 group-active:text-neutral-500" />
+              </button>
+            </div>
+          </div>
+
+          <div className="border-y border-primary py-4 my-4">
+            <p>
+              {`Words generated:
+              ${
+                showingDuplicates
+                  ? wordList.length
+                  : wordListNoDuplicates.length
+              }`}
+            </p>
+            {!showingDuplicates && (
+              <p>{`Duplicates removed: ${
+                wordList.length - wordListNoDuplicates.length
+              }`}</p>
+            )}
+          </div>
+
           <p className="whitespace-pre-line font-noto">
-            {wordList ? (
-              wordList.join('\n')
+            {wordList.length > 0 ? (
+              outputText
             ) : (
-              <span className="italic">Click generate to see word list.</span>
+              <span className="italic font-semibold">
+                Click 'Generate' to see word list.
+              </span>
             )}
           </p>
         </div>
